52aae11317903228d95f55d76b58934c0c5136a2
Shubham Sharma <shubhamku044@gmail.com>
2023-07-30 02:12:45 +0530

    fixed sizing

--- src/components/sections/SolutionForSociety.tsx
+++ src/components/sections/SolutionForSociety.tsx
@@ -12,19 +12,26 @@ const SolutionForSociety = () => {
             width={500}
           />
         </div>
-        <div className='right text-5xl max-w-2xl flex items-start justify-start flex-col '>
-          <h1 className='text-custom-blue '>The solution for Society:</h1>
-          <h1 className=''>True Ownership and freedom</h1>
+        <div className='right max-w-2xl flex items-start justify-start flex-col'>
+          <h1 className='text-5xl font-bold' style={{ lineHeight: '1.2' }}>
+            <span className='text-custom-blue'>
+              The solution for Society:
+            </span><br />
+            True Ownership and freedom
+          </h1>
           <div className='h-1 bg-custom-blue rounded w-32 mt-4 mb-4' />
-          <p className='text-left tracking-tight text-base'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Incidunt aut odit sequi. Unde illum soluta tempora id vel architecto adipisci, 
-            reprehenderit maiores animi aliquid nemo consectetur iure distinctio labore nostrum?
-            Dolorum cumque perspiciatis esse atque iusto libero placeat fugiat iste delectus odit aliquid,
-            autem dignissimos modi ipsa quam recusandae sit!
-            Incidunt aut odit sequi. Unde illum soluta tempora id vel architecto adipisci, 
-            reprehenderit maiores animi aliquid nemo consectetur iure distinctio labore nostrum?
-            Dolorum cumque perspiciatis esse atque iusto libero placeat fugiat iste delectus odit aliquid,
-            autem dignissimos modi ipsa quam recusandae sit!</p>
+          <p className='text-left tracking-tight text-lg'>
+            The incident involving Apple disabling the Bluetooth AirDrop feature in China raised
+            important questions about our rights and freedoms concerning our devices and communication
+            tools. At SendContact, we firmly believe that if you&apos;ve purchased a device and paid for
+            it with your hard-earned money, you should have complete rights and control over it. That&apos;s
+            why we provide not only a decentralized operating system but also decentralized software,
+            free from dictatorial or authoritarian control. Our Bluetooth functionality enables
+            interpersonal communication on the internet and grants you authority and control over your
+            network. With SendContact&apos;s decentralized operating system, you truly own your phone,
+            communications, data, and network, remaining free from censorship. It&apos;s time to reclaim
+            the freedom of communication and embrace a new era of empowerment.
+          </p>
         </div>
       </div>
     </section>
