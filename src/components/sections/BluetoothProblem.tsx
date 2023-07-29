d0798a46e70b1c60df552fe0f69c02380638ba38
Shubham Sharma <shubhamku044@gmail.com>
2023-07-30 02:02:43 +0530

    fix heading

--- src/components/sections/BluetoothProblem.tsx
+++ src/components/sections/BluetoothProblem.tsx
@@ -6,25 +6,29 @@ const BluetoothProblem = () => {
     <section className='bg-gray-medium text-white'>
       <div className='section flex items-center justify-evenly'>
         <div className='left text-5xl max-w-2xl flex items-start justify-start flex-col '>
-          <h1 className='text-custom-blue '> <span className='text-custom-blue'>The Problem</span> With Bluetooth</h1>
-          <h1 className=''>Technology And Our Solution</h1>
+          <h1 className='font-bold'>
+            <span className='text-custom-blue'>The Problem</span>&nbsp;
+            With Bluetooth Technology And Our Solution
+          </h1>
           <div className='h-1 bg-custom-blue rounded w-32 mt-4 mb-4' />
-          <p className='text-left tracking-normal text-base'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Incidunt aut odit sequi. Unde illum soluta tempora id vel architecto adipisci, 
-            reprehenderit maiores animi aliquid nemo consectetur iure distinctio labore nostrum?
-            Dolorum cumque perspiciatis esse atque iusto libero placeat fugiat iste delectus odit aliquid,
-            autem dignissimos modi ipsa quam recusandae sit!
-            Incidunt aut odit sequi. Unde illum soluta tempora id vel architecto adipisci, 
-            reprehenderit maiores animi aliquid nemo consectetur iure distinctio labore nostrum?
-            Dolorum cumque perspiciatis esse atque iusto libero placeat fugiat iste delectus odit aliquid,
-            autem dignissimos modi ipsa quam recusandae sit!</p>
+          <p className='text-left tracking-normal text-lg'>
+            We&apos;ve all experienced the chaos of Bluetooth technology. When turning on the
+            Bluetooth feature on your mobile phone, you&apos;re bombarded with a long list of nearby
+            devices, making it challenging to find the one you&apos;re seeking. But fear not, as SendContact
+            has developed a patented solution. Our innovative technology measures the signal strength of each
+            device in proximity to you and ensures that the closest device appears at the top of the
+            Bluetooth list. This simple yet crucial feature saves valuable time and enhances your user experience,
+            especially in crowded events or networking gatherings. With SendContact, the chaos of Bluetooth
+            scanning is effectively managed, presenting the nearest individuals in a continuous flow on your phone.
+            Just imagine meeting someone at an event and simply holding up your phones to instantly exchange
+            contact information. SendContact simplifies Bluetooth connectivity, making networking a breeze.
+          </p>
         </div>
         <div className='right'>
           <Image src={'/Bluetooth2.png'}
             alt='Send' className='bg-blend max-w-[400px]'
             height={200}
             width={200}
-            // objectFit="contain" layout="fill"
           />
         </div>
       </div>
--- src/components/sections/JoinMission.tsx
+++ src/components/sections/JoinMission.tsx
@@ -9,16 +9,16 @@ const JoinMission = () => {
           <h1 className='uppercase text-xl md:text-2xl lg:text-3xl font-semibold'> <span className='text-custom-blue'>Join Our Mission
           </span> For a Better World</h1>
           <div className='underline h-[3px] w-[130px] bg-custom-blue my-2'></div>
-          <p className='text-left tracking-tighter'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          <p className='text-left tracking-tighter text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Temporibus voluptates est laboriosam, eveniet totam sit quibusdam similique facere! Saepe nihil
-             repellat nisi esse id corrupti rerum perferendis eligendi delectus itaque debitis tenetur maxime 
-             voluptatibus adipisci fugiat quos expedita consequatur velit, blanditiis commodi molestiae nostrum! 
-             Magni unde maiores quibusdam incidunt numquam!</p>
+            repellat nisi esse id corrupti rerum perferendis eligendi delectus itaque debitis tenetur maxime
+            voluptatibus adipisci fugiat quos expedita consequatur velit, blanditiis commodi molestiae nostrum!
+            Magni unde maiores quibusdam incidunt numquam!</p>
         </div>
         <div className='right  '>
           <div className='images relative pt-[100px]'>
-            <Image className='absolute top-[10px] md:right-28 w-[300px] sm:w-[300px]'  src="/images/send_contact.jpeg" alt='' width={300} height={300}/>
-            <Image className='w-[300px] sm:w-[350px]' src="/images/join_mission.jpeg" alt='' width={350} height={350}/>
+            <Image className='absolute top-[10px] md:right-28 w-[300px] sm:w-[300px]' src="/images/send_contact.jpeg" alt='' width={300} height={300} />
+            <Image className='w-[300px] sm:w-[350px]' src="/images/join_mission.jpeg" alt='' width={350} height={350} />
           </div>
         </div>
       </div>
