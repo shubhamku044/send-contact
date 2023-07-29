a7827fba0500732722e8a0cb25bceaa73c8faec6
Shubham Sharma <shubhamku044@gmail.com>
2023-07-30 03:00:00 +0530

    fixed sizing and updated content

--- src/components/sections/DownloadSendContact.tsx
+++ src/components/sections/DownloadSendContact.tsx
@@ -7,24 +7,28 @@ const DownloadSendContact = () => {
     <section className='bg-gray-medium text-white'>
       <div className='section flex justify-between items-center'>
         <div className='left video'>
-          <Image src={'/video.jpeg'} alt='' height={550} width={550} className='rounded-3xl'/>
+          <Image src={'/video.jpeg'} alt='' height={550} width={550} className='rounded-3xl' />
         </div>
-        <div className='right text-4xl max-w-[400px] mx-auto '>
-          <h1>SendContact Brings Out The Best In Me. It's</h1>
-          <h1 className='text-custom-blue'>Simple And Easy To Use</h1>
+        <div className='right mx-auto max-w-lg'>
+          <h1 className='font-bold text-5xl' style={{ lineHeight: 1.2 }}>
+            SendContact Brings Out The Best In Me. It&apos;s <br />
+            <span className='text-custom-blue'>Simple And Easy To Use</span>
+          </h1>
           <div className='h-1 bg-custom-blue rounded w-28 mt-4' />
-          <span className='text-base'>Download SendContact App</span>
-          <div className='social-media mt-4 flex gap-4'>
-            <Link href={'/'}>
-              <div>
-                <Image src={'/App_Store.svg'} alt='App store icon' width={163} height={48} />
-              </div>
-            </Link>
-            <Link href={'/'}>
-              <div>
-                <Image src={'/Google_Play.svg'} alt='App store icon' width={163} height={48} />
-              </div>
-            </Link>
+          <div className='mt-12'>
+            <span className='text-base mt-12'>Download SendContact App</span>
+            <div className='social-media mt-4 flex gap-4'>
+              <Link href={'/'}>
+                <div>
+                  <Image src={'/App_Store.svg'} alt='App store icon' width={163} height={48} />
+                </div>
+              </Link>
+              <Link href={'/'}>
+                <div>
+                  <Image src={'/Google_Play.svg'} alt='App store icon' width={163} height={48} />
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
