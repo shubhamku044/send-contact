2b72faf3294d22f7c002d6d0ac6ef2a97a2c374f
Shubham Sharma <shubhamku044@gmail.com>
2023-07-30 17:27:31 +0530

    fixed error

--- src/components/sections/CustomerReview.tsx
+++ src/components/sections/CustomerReview.tsx
@@ -1,48 +1,63 @@
 'use client';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 import React, { useRef, useState } from 'react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import Image from 'next/image';
-import {AiOutlineArrowRight } from 'react-icons/ai';
-import {AiOutlineArrowLeft} from 'react-icons/ai';
-const data=[{id:1, 
-  title:'abcde xyzt', role:'author',ratings:'start',desc:'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'},
-{id:1, 
-  title:'abcde xyzt',
-  role:'author',
-  ratings:'start',
-  desc:'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'},
-{id:2, 
-  title:'abcde xyzt',
-  role:'author',
-  ratings:'start',
-  desc:'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'},
-{id:3, 
-  title:'abcde xyzt',
-  role:'author',
-  ratings:'start',
-  desc:'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'},
-{id:4, 
-  title:'abcde xyzt',
-  role:'author',
-  ratings:'start',
-  desc:'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'},
-{id:5, 
-  title:'abcde xyzt',
-  role:'author',
-  ratings:'start',
-  desc:'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'},
-{id:6, 
-  title:'abcde xyzt',
-  role:'author',
-  ratings:'start',
-  desc:'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'},
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+const data = [
+  {
+    id: 1,
+    title: 'abcde xyzt', role: 'author', ratings: 'start', desc: 'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'
+  },
+  {
+    id: 1,
+    title: 'abcde xyzt',
+    role: 'author',
+    ratings: 'start',
+    desc: 'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'
+  },
+  {
+    id: 2,
+    title: 'abcde xyzt',
+    role: 'author',
+    ratings: 'start',
+    desc: 'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'
+  },
+  {
+    id: 3,
+    title: 'abcde xyzt',
+    role: 'author',
+    ratings: 'start',
+    desc: 'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'
+  },
+  {
+    id: 4,
+    title: 'abcde xyzt',
+    role: 'author',
+    ratings: 'start',
+    desc: 'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'
+  },
+  {
+    id: 5,
+    title: 'abcde xyzt',
+    role: 'author',
+    ratings: 'start',
+    desc: 'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'
+  },
+  {
+    id: 6,
+    title: 'abcde xyzt',
+    role: 'author',
+    ratings: 'start',
+    desc: 'lorem ipsum dolor sit amet lorem reso saopaf ain afaagi asf asfasfnaadfg sdfd sdfg'
+  },
 ];
 
 const CustomerReview = () => {
-  const [swiperRef, setSwiperRef] = useState(null);
+  const [swiperRef, setSwiperRef] = useState<SwiperClass>();
   const nextHandler = () => {
     swiperRef?.slideNext();
   };
@@ -51,19 +66,18 @@ const CustomerReview = () => {
   };
   return (
     <div className='bg-gray-medium text-white'>
-    
+
       <section className='section'>
         <h1 className='text-5xl text-center mb-8'>Customer Reviews</h1>
         <>
           <Swiper
-            onSwiper={(swiper) => setSwiperRef(swiper)}
+            onSwiper={setSwiperRef}
             slidesPerView={4}
             spaceBetween={30}
             loop={true}
             pagination={{
               clickable: true,
             }}
-            // modules={[Pagination]}
             className="mySwiper"
           >
             {data.map((item, id) => {
@@ -78,16 +92,16 @@ const CustomerReview = () => {
                     >
                       <div className='top flex gap-6'>
                         <div className='relative h-100 w-100'>
-                          <Image 
-                            className='' 
-                            src={'/user.jpeg'} alt='user' 
+                          <Image
+                            className=''
+                            src={'/user.jpeg'} alt='user'
                             fill={true}
                           />
                         </div>
                         <div className='details flex flex-col'>
                           <h3 className='text-xl font-bold mt-4'>{item.title}</h3>
-                          <p>{item.role }</p>
-                          <Image src={'/star.jpg'} height={20} width={50} alt='ratings'/>
+                          <p>{item.role}</p>
+                          <Image src={'/star.jpg'} height={20} width={50} alt='ratings' />
                         </div>
                       </div>
                       <p className='text-center text-gray-400 mt-6'>{item.desc}</p>
@@ -98,10 +112,10 @@ const CustomerReview = () => {
             })}
             <div className='btn flex w-[100%] justify-center gap-6 my-8'>
               <div className="swiper__arrow h-[40px] w-[40px] bg-gray-light rounded-full flex justify-center items-center" onClick={preHandler}>
-                <AiOutlineArrowLeft/>
+                <AiOutlineArrowLeft />
               </div>
               <div className="swiper__arrow h-[40px] w-[40px] bg-custom-blue rounded-full flex justify-center items-center" onClick={nextHandler}>
-                <AiOutlineArrowRight/>
+                <AiOutlineArrowRight />
               </div>
             </div>
           </Swiper>
