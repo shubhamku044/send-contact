1bb628a2ea612c4b6859856e51867bb319ff81a7
Asif <asif.a.7277@gmail.com>
2023-07-30 11:32:33 +0530

    6 download sendcontact (#21)
    * download sendcontact desktoop view
    
    * fixed sizing and updated content
    
    ---------
    
    Co-authored-by: Shubham Sharma <shubhamku044@gmail.com>

--- src/components/sections/DownloadSendContact.tsx
+++ src/components/sections/DownloadSendContact.tsx
@@ -5,18 +5,19 @@ import React from 'react';
 const DownloadSendContact = () => {
   return (
     <section className='bg-gray-medium text-white'>
-      <div className='section flex justify-between items-center'>
+      <div className='section flex flex-col gap-6 md:justify-between items-center md:flex-row'>
         <div className='left video'>
           <Image src={'/video.jpeg'} alt='' height={550} width={550} className='rounded-3xl' />
         </div>
         <div className='right mx-auto max-w-lg'>
-          <h1 className='font-bold text-5xl' style={{ lineHeight: 1.2 }}>
+
+          <h1 className='font-bold text-3xl lg:text-5xl' style={{ lineHeight: 1.2 }}>
             SendContact Brings Out The Best In Me. It&apos;s <br />
             <span className='text-custom-blue'>Simple And Easy To Use</span>
           </h1>
-          <div className='h-1 bg-custom-blue rounded w-28 mt-4' />
-          <div className='mt-12'>
-            <span className='text-base mt-12'>Download SendContact App</span>
+          <div className='h-1 bg-custom-blue rounded w-28 mt-4 hidden md:block' />
+          <div className='mt-6 md:mt-8 lg:mt-12'>
+            <span className='text-base'>Download SendContact App</span>
             <div className='social-media mt-4 flex gap-4'>
               <Link href={'/'}>
                 <div>
