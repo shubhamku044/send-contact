8909d41ff7dce8cb574d23b30f7dd1985c0a8f3b
Agrim <agrimsharma1714@gmail.com>
2023-07-30 19:07:20 +0530

    bluetooth section

--- src/components/sections/BluetoothDiscover.tsx
+++ src/components/sections/BluetoothDiscover.tsx
@@ -6,87 +6,25 @@ const BluetoothDiscover = () => {
     <div className="relative h-screen">
       <div className="section flex flex-col-reverse items-center justify-between md:flex-row">
         <Image
-          src={'/coposicao-chi.png'}
+          src={'/Bluetooth Discovery Technology.png'}
           layout="fill"
           objectFit="cover"
           alt={''}
         />
-        <Image
-          src="/bluetooth wave.png"
-          width={900}
-          height={500}
-          alt={''}
-          className="mt-80 ml-28 z-[100]"
-        />
-        <Image
-          src={'/mobile.png'}
-          width={1200}
-          height={500}
-          alt={''}
-          className="z-[100] absolute left-20"
-        />
-        <Image
-          src={'/13 pro - 4.png'}
-          width={105}
-          height={100}
-          alt={''}
-          className="z-[90] -mt-8 ml-[6px] absolute left-20"
-        />
-        <Image
-          src={'/fg.jpg'}
-          width={105}
-          height={100}
-          alt={''}
-          className="z-[90] mt-7 absolute right-[75px]"
-        />
-        <Image
-          src={'/fg.jpg'}
-          width={51}
-          height={50}
-          alt={''}
-          className="z-[90] absolute left-[424px] mt-[98px]"
-        />
-        <Image
-          src={'/fg.jpg'}
-          width={40}
-          height={35}
-          alt={''}
-          className="z-[90] absolute left-[540px] mt-[137px]"
-        />
-        <Image
-          src={'/fg.jpg'}
-          width={50}
-          height={35}
-          alt={''}
-          className="z-[90] absolute right-[397px] mt-[58px]"
-        />
-        <Image
-          src={'/fg.jpg'}
-          width={40}
-          height={35}
-          alt={''}
-          className="z-[90] absolute right-[645px] mt-[12px]"
-        />
-        <Image
-          src={'/fg.jpg'}
-          width={40}
-          height={35}
-          alt={''}
-          className="z-[90] absolute right-[530px] mt-[110px]"
-        />
-
-        <div className="absolute top-10 w-full text-center">
-          <div className="section right max-w-2xl flex items-start justify-start flex-col">
+        <div className="flex justify-between  ">
+          <div>
             <h1
-              className="absolute top-10 text-3xl sm:text-5xl text-white font-bold text-left"
+              className="absolute top-9 left-16 text-3xl sm:text-5xl text-white font-bold text-left"
               style={{ lineHeight: '1.2' }}
             >
-              <span className="text-custom-blue">Bluetooth</span> Discovery{' '}
+              <span className="text-custom-blue">Bluetooth</span> Discovery{" "}
               <br />
               Technology
             </h1>
-            <div className="h-1 bg-custom-blue rounded w-36 mt-14" />
-            <p className="text-left text-base tracking-tight text-white absolute top-12 right-16 w-1/2">
+            <div className="absolute top-36 left-16 h-1 bg-custom-blue rounded w-36 " />
+          </div>
+          <div>
+            <p className="text-left text-base tracking-tight text-white absolute md:top-12 top-52 left-16 md:left-auto right-0 md:right-16 w-3/4 md:w-1/2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
               delectus sed aliquam adipisci alias aut, voluptatem corrupti,
               labore temporibus possimus dolorum nihil debitis, praesentium
