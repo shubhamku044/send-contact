442b08349bfaa28152f04bfe00f45f29e8f5c316
Shubham Sharma <shubhamku044@gmail.com>
2023-08-02 16:41:46 +0530

    Bluetooth update (#58)
    * fix text in bluetooth section
    
    * make image move to right a little bit so hand with phone will be visible, make all background as gif so now animations with phones works
    
    * phones animation now as a gif, now whole background
    
    * update text in bluetooth discover
    
    * eslint fix
    
    ---------
    
    Co-authored-by: nikolaimurzin <nikolaimurzin11@gmail.com>

--- src/components/sections/BluetoothDiscover.tsx
+++ src/components/sections/BluetoothDiscover.tsx
@@ -35,20 +35,21 @@ const BluetoothDiscover = () => {
             />
           </div>
         </div>
-        <div className="relative flex flex-row justify-between z-10">
+        <div className={styles.BluetoothText}>
           <div>
             <h1
-              className="relative top-9 text-3xl sm:text-5xl text-white font-bold text-left"
+              className={styles.BluetoothText__header + ' text-3xl sm:text-5xl text-white font-bold text-left'}
               style={{ lineHeight: '1.2' }}
             >
-              <span className="text-custom-blue">Bluetooth</span> Discovery{' '}
-              <br />
+              <span className="text-custom-blue">Bluetooth</span>
+              <div className="BluetoothText__discovery">Discovery{' '}
+                <div className={styles.BluetoothText__line + ' h-1 bg-custom-blue rounded w-36 '} />
+              </div>
               Technology
             </h1>
-            <div className="absolute top-36 left-16 h-1 bg-custom-blue rounded w-36 " />
           </div>
           <div>
-            <p className="text-left  text-justify text-base tracking-tight text-white relative md:top-12 top-52 w-3/4 md:w-1/2 ml-auto md:mr-24">
+            <p className={styles.BluetoothText__description + ' text-justify text-base tracking-tight text-white'}>
               But that&apos;s not all – our app goes even further. It incorporates
               Bluetooth discovery technology, intelligently organizing a list of
               people around you. This ensures efficient information sharing by
