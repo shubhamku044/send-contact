f2c7fb0743e0da96b394264544efa0c2f44b9a68
nikolaimurzin <nikolaimurzin11@gmail.com>
2023-08-02 08:01:17 +0000

    fix text in bluetooth section

--- src/components/sections/BluetoothDiscover.tsx
+++ src/components/sections/BluetoothDiscover.tsx
@@ -28,10 +28,10 @@ const BluetoothDiscover = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-between">
+        <div className="relative flex flex-row justify-between">
           <div>
             <h1
-              className="absolute top-9 left-16 text-3xl sm:text-5xl text-white font-bold text-left"
+              className="relative top-9 text-3xl sm:text-5xl text-white font-bold text-left"
               style={{ lineHeight: '1.2' }}
             >
               <span className="text-custom-blue">Bluetooth</span> Discovery{' '}
@@ -41,7 +41,7 @@ const BluetoothDiscover = () => {
             <div className="absolute top-36 left-16 h-1 bg-custom-blue rounded w-36 " />
           </div>
           <div>
-            <p className="text-left text-base tracking-tight text-white absolute md:top-12 top-52 left-16 md:left-auto right-0 md:right-16 w-3/4 md:w-1/2">
+            <p className="text-left  text-justify text-base tracking-tight text-white relative md:top-12 top-52 w-3/4 md:w-1/2 ml-auto md:mr-24">
               But that&apos;s not all – our app goes even further. It incorporates
               Bluetooth discovery technology, intelligently organizing a list of
               people around you. This ensures efficient information sharing by
@@ -51,15 +51,6 @@ const BluetoothDiscover = () => {
               simple way to share your business.
             </p>
           </div>
-        </div>
-        <div className="text-left md:w-1/2 text-base sm:text-lg z-[1]">
-          But that&apos;s not all – our app goes even further. It incorporates
-          Bluetooth discovery technology, intelligently organizing a list of
-          people around you. This ensures efficient information sharing by
-          presenting the nearest and most relevant individuals. Our app helps
-          you connect with potential customers with a SendContact virtual card,
-          NFC digital business card, Bluetooth connect, and a simple way to
-          share your business.
         </div>
       </div>
     </div>
